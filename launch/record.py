de261a53a45b3d5c8b8d198c4f7c0f359b0a4908
creeper5820 <cqq1960180796@163.com>
2024-07-30 07:25:51 +0000

    tidy CMakeLists.txt && add record launch

---
+++ launch/record.py
@@ -0,0 +1,31 @@
+from launch.actions import ExecuteProcess
+from launch import LaunchDescription
+from datetime import datetime
+
+year = datetime.now().year
+mouth = datetime.now().month
+day = datetime.now().day
+hour = datetime.now().hour
+minute = datetime.now().minute
+
+bag = f"/[record][{year}-{mouth}-{day}-{hour}-{minute}]"
+
+
+def generate_launch_description():
+    battlefield = ExecuteProcess(
+        cmd=[
+            "ros2",
+            "bag",
+            "record",
+            "--output",
+            bag,
+            "/livox/lidar",
+            "/livox/imu",
+        ],
+        output="screen",
+    )
+
+    launch = LaunchDescription()
+    launch.add_action(battlefield)
+
+    return launch
